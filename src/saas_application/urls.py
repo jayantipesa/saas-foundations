cd6fdb026fec96e7a87529a1a61b5856242ad62e
jayantipesa <jayanti.pesa@gmail.com>
2025-06-29 19:27:04 +0530

    storing data in session and using it for protected pages

--- src/saas_application/urls.py
+++ src/saas_application/urls.py
@@ -18,7 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from auth import views as auth_views
-from .views import home_view, about_view
+from .views import (
+    home_view,
+    about_view,
+    pw_protected_view,
+    user_only_view
+)
 
 urlpatterns = [
     path('', home_view, name='home_page'),
@@ -26,5 +31,7 @@ urlpatterns = [
     path('login/', auth_views.login_view, name='login_view'),
     path('register/', auth_views.register_view, name='register_view'),
     path('about/', about_view, name='about_page'),
+    path('protected/user-only', user_only_view, name='protected'),
+    path('protected/', pw_protected_view, name='protected'),
     path('admin/', admin.site.urls),
 ]
--- src/saas_application/views.py
+++ src/saas_application/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from visits.models import PageVisit
 
@@ -21,3 +22,26 @@ def about_view(request, *args, **kwargs):
     }
     PageVisit.objects.create(path=request.path)
     return render(request, 'home.html', context=context)
+
+
+def pw_protected_view(request, *args, **kwargs):
+    one_time_password = 'abc123'
+    is_allowed = request.session.get('protected_page_allowed') or 0
+    if request.method == 'POST':
+        user_pw_sent = request.POST.get('code') or None
+        if not request.user.is_authenticated:
+            if user_pw_sent == one_time_password:
+                return render(request, 'protected/view.html', {})
+        else:
+            is_valid = request.user.check_password(user_pw_sent)
+            if is_valid:
+                is_allowed = 1
+                request.session['protected_page_allowed'] = is_allowed
+    if is_allowed:
+        return render(request, 'protected/view.html', {})
+    return render(request, 'protected/entry.html', {})
+
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    return render(request, 'protected/user-only.html', {})
