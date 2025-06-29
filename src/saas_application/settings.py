a3dce9d14d2532160c966d26cf8759a8778077ee
jayantipesa <jayanti.pesa@gmail.com>
2025-06-29 19:27:03 +0530

    Added whitenoise to serve static files on prod

--- src/saas_application/settings.py
+++ src/saas_application/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,6 +158,11 @@ STATICFILES_DIRS = [
 # output for manage.py collectstatic
 # local-cdn
 STATIC_ROOT = BASE_DIR / 'local-cdn'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
