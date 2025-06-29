ac5b584dc9afa3a080c1e34a5c6376e37054d0f6
jayantipesa <jayanti.pesa@gmail.com>
2025-06-29 19:27:03 +0530

    Changes to prepare code for production push

--- src/saas_application/views.py
+++ src/saas_application/views.py
@@ -3,10 +3,21 @@ from django.shortcuts import render
 from visits.models import PageVisit
 
 def home_view(request, *args, **kwargs):
-    page_visit_qs = PageVisit.objects.filter(path=request.path)
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
+    all_page_visit_qs = PageVisit.objects.all()
+    page_visit_count = all_page_visit_qs.filter(path=request.path).count()
+    try:
+        count_percentage = (page_visit_count * 100.0) / all_page_visit_qs.count()
+    except:
+        count_percentage = 0
     context = {
         'name': 'Jayanti',
-        'page_visit_count': page_visit_qs.count()
+        'total_page_visit_count': all_page_visit_qs.count(),
+        'page_visit_count': page_visit_count,
+        'percentage': round(count_percentage, 0)
     }
     PageVisit.objects.create(path=request.path)
     return render(request, 'home.html', context=context)
